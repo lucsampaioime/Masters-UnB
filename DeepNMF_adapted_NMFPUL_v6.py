bc6a4e112209fd5f017d65b1d27eb183680dc14a
lucsampaioime <lucsampaioime@gmail.com>
2024-04-02 11:22:28 -0300

    Experiments

--- DeepNMF_adapted_NMFPUL_v6.py
+++ DeepNMF_adapted_NMFPUL_v6.py
@@ -230,7 +230,7 @@ def predict_classes(reconstructed_V, threshold=0.5):
 def nmf_with_update(V, n_topics, n_labeled, max_iter, tol, W, H):
 
     # Path to your CSV file
-    file_path = 'C:/Users/lucsa/Dropbox/Data Science/Mestrado UNB/Dissertação/Experimentos/Testes/Deep NMF/Datasets/tr12.mat.csv'
+    file_path = 'C:/Users/lucsa/Dropbox/Data Science/Mestrado UNB/Dissertação/Experimentos/Testes/Deep NMF/Datasets/CSTR.csv'
     V, classes = read_and_process_csv(file_path)
 
     # Rotula uma quantidade n_labeled de documentos da classe positiva
@@ -312,7 +312,7 @@ def nmf_with_update(V, n_topics, n_labeled, max_iter, tol, W, H):
 def main():
 
     # Path to your CSV file
-    file_path = 'C:/Users/lucsa/Dropbox/Data Science/Mestrado UNB/Dissertação/Experimentos/Testes/Deep NMF/Datasets/tr12.mat.csv'
+    file_path = 'C:/Users/lucsa/Dropbox/Data Science/Mestrado UNB/Dissertação/Experimentos/Testes/Deep NMF/Datasets/CSTR.csv'
     V, classes = read_and_process_csv(file_path)
 
     # Ensuring the number of documents matches the number of class labels
